7aa162e79e3e268cbd1f7992d6a2c4a728f9bc4b
Cassie Chan <cassandrachan@blueyonder.co.uk>
2019-01-19 23:35:40 +0000

    new commit

--- Task2A.py
+++ Task2A.py
@@ -26,7 +26,7 @@ def run():
     # Alternative implemention
     # for station in [s for s in stations if s.name in names]:
     #     print("Station name and current level: {}, {}".format(station.name,
-    #                                                           station.latest_level))
+    #            h                                               station.latest_level))
 
 
 if __name__ == "__main__":

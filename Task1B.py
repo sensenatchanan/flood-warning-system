10ce0ee5fc6812d4b4cb21f5e44b8378b6c4e731
sense.natchanan <sense.natchanan@gmail.com>
2019-01-20 13:42:49 +0000

    draft of 1b

---
+++ Task1B.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem import geo, stationdata
+
+
+def run():
+    """Requirements for Task 1B"""
+    stations = geo.stations_by_distance(stationdata.build_station_list(), (52.2053, 0.1218))
+    results = [(s.name, s.town, distance) for (s, distance) in stations]
+    print(results[:10])
+    print(results[-10:])
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,12 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def stations_by_distance(stations, p):
+    result = []
+    (x0, y0) = p
+    for station in stations:
+        (x1, y1) = station.coord
+        distance = ((x0 - x1)**2 + (y0 - y1)**2)**(1/2)
+        result.append((station, distance))
+    return sorted_by_key(result, 1)

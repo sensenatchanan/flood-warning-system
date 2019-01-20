7b5240d0daabd158e7d83dc50dd1ff4efee0a050
Cassie Chan <cassandrachan@blueyonder.co.uk>
2019-01-20 16:03:59 +0000

    task 1C with an error

---
+++ Task1C.py
@@ -0,0 +1,13 @@
+from floodsystem import geo, stationdata
+
+
+def run():
+    """Requirements for Task 1C"""
+    stations = geo.stations_within_radius(stationdata.build_station_list(), (52.2053, 0.1218), 10)
+    output = [(s.name) for (s) in stations]
+    print(output)
+
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,8 +10,19 @@ from .utils import sorted_by_key  # noqa
 from haversine import haversine
 
 def stations_by_distance(stations, p):
+    """ Function that calculates the distance from the stations to a point and sorts them """
     result = []
     for station in stations:
         distance = haversine(p, station.coord)
         result.append((station, distance))
+    #sort by distance
     return sorted_by_key(result, 1)
+
+def stations_within_radius(stations, centre, r):
+    """ function that returns a list of all stations within a certain radius of a point """
+    output = []
+    for station in stations:
+        distance = haversine(centre, station.coord)
+        if distance <= r:
+            output.append(station)
+    return output

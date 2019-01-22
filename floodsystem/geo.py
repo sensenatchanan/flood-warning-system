5a827488c8406c1115e2aa73e2c143e7ce8e61d4
sense.natchanan <sense.natchanan@gmail.com>
2019-01-22 15:51:40 +0000

    trying to do 1d

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -30,3 +30,12 @@ def stations_within_radius(stations, centre, r):
 def rivers_with_stations(stations):
     output = set((s.river) for (s) in stations)
     return output
+
+def stations_by_river(stations):
+   #rivers = set((s.river) for (s) in stations)
+    #data = set(((s.river),(s.name)) for (s) in stations)
+    results = [[s.name, s.town] for (s) in stations]
+
+
+    #d = {(s.river):(s.name) for (s) in stations}
+    return results

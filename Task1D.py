80cf63e3de639d8f86d6e4daebacdb294137dfbc
sense.natchanan <sense.natchanan@gmail.com>
2019-01-22 17:24:41 +0000

    cleaned 1d

--- Task1D.py
+++ Task1D.py
@@ -5,13 +5,17 @@ def run():
     """Requirements for Task 1D"""
     output= geo.rivers_with_stations(stationdata.build_station_list())
     sorted_output = sorted(output)
-    print(sorted_output[:10])
-    print('\n number of rivers with at least one station is',  len(output))
+
+    #Print no. of rivers with stations
+    print('\nNumber of rivers with at least one station is',  len(output), ". \n")
+    #Print first 10 stations
+    print("First 10 rivers with stations: \n", sorted_output[:10])
     rivers = geo.stations_by_river(stationdata.build_station_list())
     
-    print('River Aire:', sorted(rivers["River Aire"]))
-    print('\n River Cam:', sorted(rivers["River Cam"]))
-    print('\n Thames:', sorted(rivers["River Thames"]))
+    #Print stations on the following rivers
+    print('\nRiver Aire: \n', sorted(rivers["River Aire"]))
+    print('\nRiver Cam: \n', sorted(rivers["River Cam"]))
+    print('\nThames: \n', sorted(rivers["River Thames"]))
 
 
 
